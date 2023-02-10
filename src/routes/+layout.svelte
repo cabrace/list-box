857e0d87657f60956410874fcfb05e760413fbe9
CaBrace <christopher.a.brace@gmail.com>
2023-02-09 22:49:44 -0400

    Initial commit: TailwindCSS + SkeletonUI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	//Essential
+	import '@skeletonlabs/skeleton/themes/theme-vintage.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+
+	import { itemStore } from '$lib/stores/stores.js';
+
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+
+	let list;
+	let items = [
+		{ name: 'Home', id: '0', list: ['Water Plants', 'Do Cooking'] },
+		{ name: 'Work/School', id: '1', list: ['Complete Documentation'] },
+		{ name: 'Personal', id: '2', list: ['Drink Water', 'Meditate', 'Exercise'] }
+	];
+
+	$: console.log($itemStore[0]);
+	let selectedItems = items[1];
+
+	$: itemStore.set(selectedItems.list);
+	$: console.log($itemStore);
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header" />
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Left Sidebar -->
+		<div class="flex h-full text-center flex-col">
+			<h1>Box</h1>
+
+			<ListBox class="text-left">
+				{#each items as item}
+					<ListBoxItem bind:group={selectedItems} name="items" value={item}>
+						{item.name}
+					</ListBoxItem>
+				{/each}
+			</ListBox>
+		</div>
+	</svelte:fragment>
+
+	<svelte:fragment slot="sidebarRight">
+		<div class="text-center border border-white-400 h-full">Sidebar Right</div>
+	</svelte:fragment>
+	<svelte:fragment slot="pageHeader">
+		<div class="flex justify-center my-4">
+			<div class="card p-2 flex flex-row ">
+				<!-- <span class="p-2 bg-primary-700 rounded-sm p-3">+</span> -->
+				<input class="input border-secondary-500" type="text" placeholder="Add something new" />
+			</div>
+		</div>
+		<!-- <div class="text-center border border-white-400">Page Header</div> -->
+	</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">
+		<div class="text-center border border-white-400">Page Footer</div>
+	</svelte:fragment>
+	<svelte:fragment slot="footer">
+		<div class="text-center border border-white-400">Footer</div>
+	</svelte:fragment>
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { itemStore } from '$lib/stores/stores.js';
+
+	let valueSingle: string = 'books';
+</script>
+
+<!-- content wrapper -->
+<!-- <div class="grid grid-cols-9">
+	<ol class="list">
+		{#each $itemStore as item,i}
+		<li>
+			<span>{i + 1}</span>
+			<span class="flex-auto">{item}</span>
+		</li>
+		{/each}
+	</ol>
+</div> -->
+
+	<!-- <div class="flex flex-row justify-center my-8"> -->
+		<!-- <div class="card p-2 flex flex-row w-1/4">
+			<span class="p-2 bg-primary-700 rounded-sm p-3">+</span>
+			<input class="input" type="text" placeholder="Add something new" />
+		</div> -->
+	<!-- </div> -->
+<div class="grid grid-cols ml-10">
+	{#each $itemStore as item, i}
+		<div class="flex flex-row">
+			<div class="card p-4 flex flex-row">
+				<span class="p-2 bg-primary-700 rounded-sm p-3">{i + 1}</span>
+				<input class="input border-primary-300 border-6" type="text" placeholder="input text" value={item} />
+			</div>
+		</div>
+	{/each}
+</div>
